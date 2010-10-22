f262df3ab2237037c8598f3e333576644a2e331e
Steve Harris <sharrissf@gmail.com>
2010-10-22 14:27:35 -0700

    some refactoring

--- src/main/java/org/sharrissf/sample/EhcacheSearchPlaying.java
+++ src/main/java/org/sharrissf/sample/EhcacheSearchPlaying.java
@@ -12,29 +12,43 @@ import net.sf.ehcache.search.Attribute;
 import net.sf.ehcache.search.Query;
 import net.sf.ehcache.search.Results;
 import net.sf.ehcache.search.aggregator.Average;
+import net.sf.ehcache.search.attribute.AttributeExtractor;
 
 import org.sharrissf.sample.Person.Gender;
 
+/**
+ * Sample app briefly showing some of the api's one can use to search in Ehcache. This has been written against the latest snapshot build so
+ * it can become outdated at any time
+ * 
+ * @author steve
+ * 
+ */
 public class EhcacheSearchPlaying {
+    private CacheManager cacheManager;
+    private Cache cache;
 
-    public static void main(String[] args) throws IOException {
+    public EhcacheSearchPlaying() {
+        initializeCache();
+    }
 
-        Configuration cacheManagerConfig = new Configuration();
-
-        // Add default cache
-        cacheManagerConfig.addDefaultCache(new CacheConfiguration());
+    private void initializeCache() {
 
         // Create Cache
-
+        Configuration cacheManagerConfig = new Configuration();
+        cacheManagerConfig.addDefaultCache(new CacheConfiguration());
         CacheConfiguration cacheConfig = new CacheConfiguration("test", -1).eternal(true);
 
+        // Create attributes on the stuff we want to be able to search on.
+
+        // You can use an expression for getting at the value to be indexed on a cache or you can code your own
         SearchAttribute sa = new SearchAttribute();
         sa.setExpression("value.getAge()");
         sa.setName("age");
         cacheConfig.addSearchAttribute(sa);
 
         sa = new SearchAttribute();
-        sa.setExpression("value.getName()");
+        sa.className("org.sharrissf.sample.EhcacheSearchPlaying$NameAttributeExtractor");
+
         sa.setName("name");
         cacheConfig.addSearchAttribute(sa);
 
@@ -45,10 +59,11 @@ public class EhcacheSearchPlaying {
 
         cacheManagerConfig.addCache(cacheConfig);
 
-        CacheManager cacheManager = new CacheManager(cacheManagerConfig);
+        cacheManager = new CacheManager(cacheManagerConfig);
+        cache = cacheManager.getCache("test");
+    }
 
-        Cache cache = cacheManager.getCache("test");
-
+    public void runTests() throws IOException {
         cache.put(new Element(1, new Person("Tim Eck", 35, Gender.MALE)));
         cache.put(new Element(2, new Person("Pamela Jones", 23, Gender.FEMALE)));
         cache.put(new Element(3, new Person("Ari Zilka", 25, Gender.MALE)));
@@ -82,7 +97,6 @@ public class EhcacheSearchPlaying {
         Query averageAgeQuery = cache.createQuery();
         averageAgeQuery.includeAggregator(new Average(), age);
         System.out.println("Average age: " + averageAgeQuery.execute().aggregateResult());
-
     }
 
     private static void read() throws IOException {
@@ -90,4 +104,17 @@ public class EhcacheSearchPlaying {
         System.in.read();
     }
 
+    public static void main(String[] args) throws IOException {
+        new EhcacheSearchPlaying().runTests();
+    }
+
+    @SuppressWarnings( { "unused", "serial" })
+    public static class NameAttributeExtractor implements AttributeExtractor {
+
+        @Override
+        public Object attributeFor(Element element) {
+            return ((Person) element.getValue()).getName();
+        }
+
+    }
 }
