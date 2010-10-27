6ba3032fe634529b98762ccc240f479f8f19d0b3
Steve Harris <sharrissf@gmail.com>
2010-10-27 15:59:48 -0700

    Make a kit for this search sample and a run script

---
+++ src/assemble/run.sh
@@ -0,0 +1,1 @@
+java -Xms200m -Xmx200m -cp ".:./ehcache-search-sample-0.0.1-SNAPSHOT.jar:./ehcache-core-2.3.1-SNAPSHOT.jar:slf4j-api-1.5.11.jar:slf4j-jdk14-1.5.11.jar" org.sharrissf.sample.EhcacheSearchPlaying
