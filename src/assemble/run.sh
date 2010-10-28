69025f806677bcff121524241a37fe3d525e67d7
Steve Harris <sharrissf@gmail.com>
2010-10-27 19:08:44 -0700

    made it easy to build via maven and run

--- src/assemble/run.sh
+++ src/assemble/run.sh
@@ -1,1 +1,1 @@
-java -Xms200m -Xmx200m -cp ".:./ehcache-search-sample-0.0.1-SNAPSHOT.jar:./ehcache-core-2.3.1-SNAPSHOT.jar:slf4j-api-1.5.11.jar:slf4j-jdk14-1.5.11.jar" org.sharrissf.sample.EhcacheSearchPlaying
+java -Xms200m -Xmx200m -cp ".:./target/ehcache-search-sample-0.0.1-SNAPSHOT.jar:./lib/ehcache-core-2.3.1-SNAPSHOT.jar:./lib/slf4j-api-1.5.11.jar:./lib/slf4j-jdk14-1.5.11.jar" org.sharrissf.sample.EhcacheSearchPlaying
