87b15918e3991dc99355821095179421fbe6ce60
Steven Harris <sharrissf@gmail.com>
2011-03-28 22:14:56 -0700

    Update for 2.4.1 release

--- src/assemble/run.sh
+++ src/assemble/run.sh
@@ -1,1 +1,1 @@
-java -Xms200m -Xmx200m -cp ".:./target/ehcache-search-sample-1.0.0.jar:./lib/ehcache-core-2.4.0.jar:./lib/slf4j-api-1.6.1.jar:./lib/slf4j-jdk14-1.6.1.jar" org.sharrissf.sample.EhcacheSearchPlaying
+java -Xms200m -Xmx200m -cp ".:./target/ehcache-search-sample-1.0.1.jar:./lib/ehcache-core-2.4.1.jar:./lib/slf4j-api-1.6.1.jar:./lib/slf4j-jdk14-1.6.1.jar" org.sharrissf.sample.EhcacheSearchPlaying
